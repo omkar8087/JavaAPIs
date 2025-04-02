571aba476681b6e27ffa84ff178d4dc5dd30063c
SalmanHashmi <132178289+Salman7040@users.noreply.github.com>
2025-04-02 21:54:38 +0530

    test list api

--- student-Management/src/main/java/com/studentmanagement/student/Management/controller/StudentController.java
+++ student-Management/src/main/java/com/studentmanagement/student/Management/controller/StudentController.java
@@ -1,8 +1,7 @@
 package com.studentmanagement.student.Management.controller;
 
 import com.studentmanagement.student.Management.Entity.StudentDetailsEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,17 +11,36 @@ public class StudentController {
 
     List<StudentDetailsEntity> studentDetail = new ArrayList<>();
 
-    @GetMapping("/getStudentDetails")
-    public List<StudentDetailsEntity> GetStudentInfo(){
-        studentDetail.add(
-        new StudentDetailsEntity(
-                101,
-                "abcd xyz",
-                "mumbai",
-                "400098",
-                1234567891)
-        );
+    @GetMapping("/get")
+    public List<StudentDetailsEntity> GetStudentInfo() {
         return studentDetail;
+    }
+
+
+
+    @GetMapping("/get/{Id}")
+    public StudentDetailsEntity GetStudentInfo(@PathVariable int Id) {
+        StudentDetailsEntity std =  studentDetail.get(Id);
+        return std;
+    }
+
+
+    @PostMapping("add")
+    public boolean addStudent(@RequestBody StudentDetailsEntity stdEntity) {
+      return studentDetail.add(stdEntity);
+    }
+
+
+    @DeleteMapping("delete/{Id}")
+    public StudentDetailsEntity deleteById(@PathVariable int Id){
+        StudentDetailsEntity std = studentDetail.remove(Id);
+        return  std;
+    }
+
+    @PutMapping("update/{Id}")
+    public StudentDetailsEntity updateById(@PathVariable int Id,@RequestBody StudentDetailsEntity std){
+          studentDetail.set(Id,std);
+        return   studentDetail.get(Id);
     }
 
 }
